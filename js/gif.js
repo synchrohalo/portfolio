30a5efdd76fcbd76291761f130460d132c97eb5b
synchrohalo <joanne.b.arboleda@gmail.com>
2017-07-17 20:11:04 -0700

    cleaned up script and code

--- js/gif.js
+++ js/gif.js
@@ -2,21 +2,21 @@ $( document ).ready(function() {
 
   var isPlaying = false;
 
-  // initialize gif playing in DT project
+  // initialize GIF playing in DT project
 	var playGifReady = function(){
 		$( ".gif-play" ).click( function(){
-      if(!isPlaying){
+      if( !isPlaying ){
 				var id = $( this ).attr( "id" );
 				$( this ).attr( "src", "../img/works/design/" + id + ".gif" );
 
         isPlaying = true;
-      }
+      } // on click, if not playing, change to to animated GIF
       else{
         var id = $( this ).attr( "id" );
 				$( this ).attr( "src", "../img/works/design/" + id + ".png" );
   
         isPlaying = false;
-      }
+      } // else, stop playing and change back to static PNG
 		});
 	}
 	
--- js/script.js
+++ js/script.js
@@ -1,24 +1,16 @@
 $( document ).ready(function() {
-  $(window).scroll(function() {
-    if( $(document).scrollTop() >= $( '#about' ).offset().top ) {
+  
+  // changes current page to About when scrolled down
+  $( window ).scroll( function() {
+    if( $( document ).scrollTop() >= $( '#about' ).offset().top ) {
       $( 'nav ul li:eq(1)' ).addClass( 'current' );
       $( 'nav ul li:eq(0)' ).removeClass( 'current' );
-    }
+    } // if at or beyond #about anchor point, switch current page to About
     else {
       $( 'nav ul li:eq(1)' ).removeClass( 'current' );
       $( 'nav ul li:eq(0)' ).addClass( 'current' );
     }
-  });
+  }); // else, switch to or remain at Home
 
-  // initialize gif playing in DT project
-	var playGifReady = function(){
-		$( ".gif-play" ).click( function(){
-				var id = $( this ).attr( "id" );
-				console.log( id );
-				
-				$( this ).attr( "src", "../img/works/design/" + id + ".gif" );
-		});
-	}
-	
 	playGifReady();
 });
